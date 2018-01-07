d550ef08ff3b8b9bcff32b9b6d2d639bcc2fca67
ArturWincenciak <teo.vincent.com@gmail.com>
2018-01-07 22:23:19 +0100

    add bookmarks for post in my blog

--- TeoVincent.Deadlock/Program.cs
+++ TeoVincent.Deadlock/Program.cs
@@ -13,18 +13,17 @@ namespace TeoVincent.Deadlock
         {
             Console.WriteLine("Let's get the started. We are in main thread");
 
-            Task.Factory.StartNew(RiseDeadlock);
-
-            Console.WriteLine("Simulate long running instruction in main thread.");
-            Thread.Sleep(5000);
-            Console.WriteLine("The long running instruction has been ended in main thread.");
+            Task.Factory.StartNew(RiseDeadlock); // #01
 
             Console.WriteLine("We are before lockB in main thread.");
-            lock (lockB)
+            lock (lockB) // #04
             {
                 Console.WriteLine("We are in lockB in main thread.");
+                Console.WriteLine("Simulate long running instruction in main thread.");
+                Thread.Sleep(5000); // #05
+                Console.WriteLine("The long running instruction has been ended in main thread.");
                 Console.WriteLine("We are waiting for release of lockA in main thread.");
-                lock (lockA)
+                lock (lockA) // #06
                 {
                     Console.WriteLine("This code will never be executed in main thread.");
                 }
@@ -37,16 +36,16 @@ namespace TeoVincent.Deadlock
         {
             Console.WriteLine("RiseDeadlock method hes been started in new thread.");
             Console.WriteLine("We are before lockA in RiseDeadlock method.");
-            lock (lockA)
+            lock (lockA) // #02
             {
                 Console.WriteLine("We are in lockA in RiseDeadlock method.");
                 Console.WriteLine("Simulate long running instruction in RiseDeadlock method.");
-                Thread.Sleep(10000);
+                Thread.Sleep(10000); // #03
                 Console.WriteLine("The long running instruction has been ended in RiseDeadlock method.");
 
                 Console.WriteLine("We are in lockA before lockB in RiseDeadlock method.");
                 Console.WriteLine("We are waiting for release of lockB in RiseDeadlock method.");
-                lock (lockB)
+                lock (lockB) // # 07
                 {
                     Console.WriteLine("This code will never be executed in RiseDeadlock method.");
                 }
